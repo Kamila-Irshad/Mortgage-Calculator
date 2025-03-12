2e6f274d433b8716c575eee1505fecc16d7116d7
Kamila-Irshad <kamilairshad366@gmail.com>
2025-03-12 19:45:24 +0500

    starting Project

--- src/app/Components/Form.jsx
+++ src/app/Components/Form.jsx
@@ -1,9 +1,16 @@
+const Form = () => {
+  return (
+    <>
+      <div className="bg-[#E3F4FC] h-screen">
+        <div className="bg-white ">
+            <h1>Mortgage Calculator</h1>
+            <form action="">
 
-const Form =()=>{
-    return (
-        <>
-        
-        </>
-    )
-}
+            </form>
+        </div>
+        <div className="bg-[#12303F]"></div>
+      </div>
+    </>
+  );
+};
 export default Form;
